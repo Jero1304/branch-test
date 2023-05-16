b6e2fd18606a9ce3471fac3ca5e09839387d7db8
GraceGenuardi <graziagenuardi96@gmail.com>
2023-05-16 11:58:04 +0200

    START fist test commit

---
+++ src/components/test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+    </div>
+</template>
+  
+<script>
+    export default {
+        data() {
+            return {
+            title: 'Eccoci nel Main!',
+            description: 'Lorem ipsum super figo che possa descrivere un main super figo e super funzionale, enjoy the main!'
+            };
+        }
+    };
+</script>
+  
+  <style scoped>
+  h1 {
+    color: green;
+  }
+
+  p {
+    color: rgb(44, 198, 44);
+  }
+
+  </style>
