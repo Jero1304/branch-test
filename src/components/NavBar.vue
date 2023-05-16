f982bbc45d2e30e24b6e9edabe2bcf8a580ee6e6
marypaduano <maria.paduano@gmail.com>
2023-05-16 12:02:02 +0200

    feat test navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="nav-bar">
+
+        <ul class="nav_item">
+            <li>ciao</li>
+            <li>ciao</li>
+            <li>ciao</li>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </ul>
+        
+
+    </section>
+    
+</template>
+  
+<script>
+export default {
+
+}
+</script>
+  
+<style scoped>
+.nav-bar{
+    background-color: aqua;
+}
+.nav_item{
+    display: flex;
+}
+ 
+</style>
